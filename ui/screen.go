0c7db28e35ce5005114544d721330ea12e9d9458
Rasmus Reiler <562749+Evilbits@users.noreply.github.com>
2025-03-23 18:14:26 +0000

    Remove old cursor movement logic

--- ui/screen.go
+++ ui/screen.go
@@ -43,18 +43,6 @@ func (screen *Screen) RenderCursor(x int, y int) {
 	screen.innerScreen.SetContent(x, y, primary, combining, cursorStyle)
 }
 
-func (screen *Screen) RenderCursorMove(x int, y int, prevX int, prevY int) {
-	cursorColor := tcell.GetColor(screen.cursorColor)
-	cursorStyle := tcell.StyleDefault.Background(cursorColor)
-
-	prevPrimary, prevCombining, prevStyle, _ := screen.innerScreen.GetContent(prevX, prevY)
-	primary, combining, _, _ := screen.innerScreen.GetContent(x, y)
-
-	// Update new cursor location and reset previous location
-	screen.innerScreen.SetContent(x, y, primary, combining, cursorStyle)
-	screen.innerScreen.SetContent(prevX, prevY, prevPrimary, prevCombining, prevStyle)
-}
-
 func (screen *Screen) Size() (width int, height int) {
 	return screen.innerScreen.Size()
 }
